9d9ab4ae6ed9bcfc15fa1f46d74ee319f75141ab
Francisco Barrios Romo <pancho@iMac-de-Francisco.local>
2017-11-23 12:02:51 -0300

    mejoras y cambios de textos

---
+++ ip.php
@@ -0,0 +1,106 @@
+<?php include("header.php"); ?>
+
+    <div>
+       <img class="imgOrbit" style="width: 100%; height:auto;" data-interchange="[assets/Slider3DesktopHD.jpg, small], [assets/Slider3DesktopHD.jpg, medium], [assets/Slider3DesktopHD.jpg, large]]">  
+    </div> 
+
+
+
+    <div class="sections">
+      <section id="second" data-magellan-target="second">
+        <div class="row text-center" > 
+            <div class="columns small-12 align-self-middle text-center" " >
+                <br>
+                <h3 style=" line-height: 1.2; padding-bottom: 20px;"><span>Lo bueno de estar juntos</span> es que <span>La Araucana</span> te da la posiblidad de que tú y tu familia puedan estudiar con descuentos especiales</h3> 
+            </div>   
+        </div>
+        <div class="row text-center" style="min-height: auto;"> 
+            <div class="columns small-12 align-self-middle " >
+              <br>
+              <h5  id="ipH5" style=" line-height: 1.2"> <span>INSTITUTOS PROFESIONALES <br> Y CENTROS DE FORMACIÓN TÉCNICA</span></h5>
+              
+            </div>   
+        </div>
+
+        <div class="row small-up-1 medium-up-3 " style="border-bottom-style: dashed; border-color: #0E3E71;">
+         
+          <div class="small-12 medium-4 columns" id="colInstitute">
+            <a target="_blank" href="http://tdigital.laaraucana.cl/beneficio/iacc-es" >
+            <div class="card columns">
+              <div class="row container" id="rowInstitute">       
+                <div class="small-4 columns card2 ">
+                        <img src="assets/ipIacc.png" class="attachment-full size-full wp-post-image" alt=""  />    
+                </div>
+                <div class="small-8 columns  card-section align-self-middle text-left" id="colInstitute">
+                      <h5>instituto profesional IACC:</h5>
+                      <p>HASTA 35% DCTO.</p>
+                      <img src="assets/plus.svg" width="50" height="auto">
+                </div>
+              </div>   
+            </div>
+            </a>
+          </div>
+
+          
+
+          <div class="small-12 medium-4 columns" id="colInstitute">
+            <a target="_blank" href="http://tdigital.laaraucana.cl/beneficio/iploslagos-es" >
+            <div class="card columns">
+              <div class="row container" id="rowInstitute" >       
+                <div class="small-4 columns card2 ">
+                        <img src="assets/ipLosLagos.png" class="attachment-full size-full wp-post-image" alt=""  />    
+                </div>
+                <div class="small-8 columns  card-section align-self-middle text-left" id="colInstitute">
+                      <h5>instituto profesional y CFT Los Lagos:</h5>
+                      <p>15% DCTO.</p>
+                      <img src="assets/plus.svg" width="50" height="auto">
+                </div>
+              </div>   
+            </div>
+            </a>
+          </div>
+        
+           <div class="small-12 medium-4 columns" id="colInstitute">
+            <a target="_blank" href="http://tdigital.laaraucana.cl/beneficio/manpower" >
+            <div class="card columns">
+              <div class="row container" id="rowInstitute" >       
+                <div class="small-4 columns card2 ">
+                        <img src="assets/ipManPower3.png" class="attachment-full size-full wp-post-image" alt=""  />    
+                </div>
+                <div class="small-8 columns  card-section align-self-middle text-left" id="colInstitute">
+                      <h5>centro de formación técnica ManPower:</h5>
+                      <p>hasta 15% DCTO.</p>
+                      <img src="assets/plus.svg" width="50" height="auto">
+                </div>
+              </div>   
+            </div>
+            </a>
+          </div>
+
+          <div class="small-12 medium-4 columns" id="colInstitute">
+            <a target="_blank" href="http://tdigital.laaraucana.cl/beneficio/icel-es" >
+            <div class="card columns">
+              <div class="row container" id="rowInstitute">       
+                <div class="small-4 columns card2 ">
+                        <img src="assets/ipIcel.png" class="attachment-full size-full wp-post-image" alt=""  />    
+                </div>
+                <div class="small-8 columns  card-section align-self-middle text-left" id="colInstitute">
+                      <h5>centro de formación técnica ICEL:</h5>
+                      <p>10% DCTO.</p>
+                      <img src="assets/plus.svg" width="50" height="auto">
+                </div>
+              </div>   
+            </div>
+            <br>
+            </a><br>
+          </div>
+
+        </div>  
+
+     </section>
+     
+    <?php include("idiomas.php"); ?>
+
+    <?php include("footer.php"); ?>
+
+  
